540370f4dabdb733652c67de04d7aef49d3f17ab
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-03-28 22:37:23 +0200

    idea

--- src/main/java/ua/in/sz/git/module/App.java
+++ src/main/java/ua/in/sz/git/module/App.java
@@ -6,7 +6,7 @@ import ua.in.sz.git.module.b.ModuleB;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("main: " + App.class.getName());
+        System.out.println("main  2222: " + App.class.getName());
 
         ModuleA.exec();
 
